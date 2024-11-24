779ca739bd99d9ed5c1d2e58d153275dfcd2cbd1
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-11-25 00:40:18 +0330

    Add File in Repo.

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1>Hey There <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand%20gestures/Waving%20Hand%20Light%20Skin%20Tone.png" alt="Waving Hand Light Skin Tone" width="35" height="35"/> Im Mr.Legend <img src="https://github.com/Tarikul-Islam-Anik/tarikul-islam-anik/raw/main/assets/images/Fire.png" alt="Slightly Smiling Face" width="35" height="35"/></h1>
+
+### movie-app-sobhan-musazadeh 👌
+
+## Project MovieApp ...
+
+<!-- CONTACT -->
+## <img src="https://github.com/Tarikul-Islam-Anik/tarikul-islam-anik/raw/main/assets/images/Globe%20with%20Meridians.png" alt="Slightly Smiling Face" width="25" height="25"/> Support Me
+
+<a href="https://t.me/soobhhan"><img src="https://github.com/gauravghongde/social-icons/blob/master/PNG/Color/Telegram.png?raw=true" width="18" title="Sobhan"> Telegram</a><br>
+<a href="https://instagram.com/sobhan.__.85"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1024px-Instagram-Icon.png" width="18" title="Sobhan"> Instagram</a><br>
+<a href="https://www.linkedin.com/in/sobhan-musazadeh"><img src="https://github.com/gauravghongde/social-icons/blob/master/PNG/Color/LinkedIN.png?raw=true" width="18" title="Sobhan"> Linkedin</a>
+
+<p align="right"><a href="#readme-top">👆</a></p>
---
+++ main.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+void main() {
+  // for (var i = 1; i <= 5; i++) {
+  //   for (var j= 1; j<=i; j++) {
+  //     print("*");
+  //   }
+  //   print("\n");
+  // }
+
+  // ! ---------------------------------------------------------------
+
+  // for (var i = 1; i <= 10; i++) {
+  //   for (var j = 1; j <= 10; j++) {
+  //     print(j*i);
+  //   }
+  //   print("\t");
+  // }
+
+  // ! ---------------------------------------------------------------
+
+  // final arryStudent = [
+  //   {"id ": 1, "name": "sobhan", "score": 60},
+  //   {"id ": 2, "name": "reza", "score": 40},
+  //   {"id ": 3, "name": "mohamad", "score": 10},
+  //   {"id ": 4, "name": "kazem", "score": 200},
+  //   {"id ": 5, "name": "shayan", "score": 90},
+  // ];
+  // var maxScore = 0;
+
+  // for (var user in arryStudent) {
+  //   int temp = int.parse(user["score"].toString());
+  //   maxScore = (maxScore < temp) ? temp : maxScore;
+  // }
+
+  // print(maxScore);
+  // ! ---------------------------------------------------------------
+
+  // var promt = 47; // * Enter Your Number
+  // var temp = 0;
+
+  // for (var i = 1; i < promt; i++) {
+  //   if (promt % i == 0) {
+  //     temp++;
+  //   }
+  // }
+  // if (temp == 1) {
+  //   print("yes");
+  // } else {
+  //   print("no");
+  // }
+
+  // ! ---------------------------------------------------------------
+
+  // var promt = 50; // * Enter Your Number
+
+  // if (promt % 2 == 0) {
+  //   print("Zoj");
+  // } else {
+  //   print("Fard");
+  // }
+  // ! ---------------------------------------------------------------
+
+  // var inputUser = int.parse(stdin.readLineSync()!);
+  // // var inputUser = 5;
+  // var factoryel = 1;
+  // for (var i = 1; i <= inputUser; i++) {
+  //   factoryel *= i;
+  // }
+
+  // print(factoryel);
+}
